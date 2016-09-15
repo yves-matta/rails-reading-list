7ece1eb1398e6be1f114f3683c3b275af9258711
Yves Matta <Yves.Matta7@gmail.com>
2016-09-15 15:02:09 -0400

    Extract json parse method

--- test/integration/listing_books_test.rb
+++ test/integration/listing_books_test.rb
@@ -13,7 +13,7 @@ class ListingBooksTest < ActionDispatch::IntegrationTest
 		assert_equal 200, response.status
 		assert_equal Mime[:json], response.content_type
 
-		assert_equal Book.count, JSON.parse(response.body).size
+		assert_equal Book.count, json(response.body).size
 	end
 
 	test 'get top rated books' do
@@ -22,7 +22,7 @@ class ListingBooksTest < ActionDispatch::IntegrationTest
 		assert_equal 200, response.status
 		assert_equal Mime[:json], response.content_type
 
-		assert_equal 1, JSON.parse(response.body).size
+		assert_equal 1, json(response.body).size
 	end
 
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,5 +7,7 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+  def json(body)
+  	JSON.parse(body)
+  end
 end

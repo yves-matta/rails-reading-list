09a3a3c10538ff9edb4d96ee886a5020453521b6
Yves Matta <Yves.Matta7@gmail.com>
2016-09-15 14:23:24 -0400

    Add book listing

---
+++ test/integration/listing_books_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class ListingBooksTest < ActionDispatch::IntegrationTest
+
+	def setup
+		Book.create!(title: 'Pragmatic program', rating: 5)
+		Book.create!(title: 'Enders game', rating: 4)
+	end
+
+	test 'get books' do
+		get '/books'
+
+		assert_equal 200, response.status
+		assert_equal Mime[:json], response.content_type
+
+		assert_equal Book.count, JSON.parse(response.body).size
+	end
+
+end
